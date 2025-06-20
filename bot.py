a221716ce5a6fd4607f55a664320394ebf29dbc7
cybersteve547 <minecraftstevso@gmail.com>
2025-06-20 12:47:05 +0000

    initial comment

--- bot.py
+++ bot.py
@@ -6,4 +6,4 @@ import os
 time = datetime.now()
 session = sa.login(os.environ['SCRATCH_USERNAME'], os.environ['SCRATCH_PASSWORD'])
 target = session.connect_user("ScratchNewsBot")
-target.post_comment(time.strftime("%Y-%m-%d %H:%M:%S"))
+target.post_comment(time.strftime("%Y-%m-%d))
