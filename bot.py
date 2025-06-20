e2a12d6dda087c2311d52b09f3cf5695993160d5
cybersteve547 <minecraftstevso@gmail.com>
2025-06-20 14:05:16 +0000

    now were getting somewhere

--- bot.py
+++ bot.py
@@ -1,9 +1,25 @@
 
 import scratchattach as sa
 from datetime import datetime
+import time
 import os
 
-time = datetime.now()
+t = datetime.now()
+
+print("logging in...")
 session = sa.login(os.environ['SCRATCH_USERNAME'], os.environ['SCRATCH_PASSWORD'])
+print("logged in!")
+
+print("getting data...")
 target = session.connect_user("ScratchNewsBot")
-comment = target.post_comment(time.strftime("SCRATCH NEWS %m-%d-%Y Source code: https://github.com/cybersteve547/ScratchNewsBot Hello Scratchers! Below you will find info about some things happening today!"))
+featured = sa.featured_projects[0]
+
+print("preparing comments...")
+c1 = t.strftime("SCRATCH NEWS %m-%d-%Y Source code: https://github.com/cybersteve547/ScratchNewsBot Hello Scratchers! Below you will find info about some things happening today!")
+c2 = "Today's featured project is " + featured.title + " by " + featured.author_name + "!"
+
+print(c1)
+comment = target.post_comment(c1)
+time.sleep(120)
+print(c2)
+comment.reply(c2)
