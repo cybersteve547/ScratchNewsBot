9151fd22ad2c967e7daabf34448256153a395bc7
cybersteve547 <minecraftstevso@gmail.com>
2025-06-20 12:47:34 +0000

    oops forgot this

--- bot.py
+++ bot.py
@@ -6,4 +6,4 @@ import os
 time = datetime.now()
 session = sa.login(os.environ['SCRATCH_USERNAME'], os.environ['SCRATCH_PASSWORD'])
 target = session.connect_user("ScratchNewsBot")
-target.post_comment(time.strftime("%Y-%m-%d))
+comment = target.post_comment(time.strftime("SCRATCH NEWS %m-%d-%Y Source code: https://github.com/cybersteve547/ScratchNewsBot Hello Scratchers! Below you will find info about some things happening today!)
